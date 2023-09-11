19f20ae58a6e06ce7179937a29e25d65bf1a62b9
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-11 18:05:36 +0000

    final

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
-
+require "active_support/all"
 get("/square/new") do
 erb(:square)
 end
@@ -22,11 +22,28 @@ get("/payment/new") do
   erb(:payment)
 end
 get("/payment/results") do
-  
+  apr_raw = params.fetch("user_apr").to_f
+  @aprstring = apr_raw.to_fs(:percentage, { :precision => 4 })
+  @years = params.fetch("user_years").to_f
+  @pv = params.fetch("user_pv").to_f
+
+  n = @years * 12
+  apr = apr_raw / 100
+  r = apr/12
+  numerator = r * @pv
+  denominator = 1- ((1+r)**-n)
+  payment = numerator/denominator
+  @paymentstring  =  payment.to_fs(:currency)  
   erb(:calc)
 end
 get("/random/new") do
   erb(:random)
+end
+get("/random/results") do
+  @min = params.fetch("min").to_f
+  @max = params.fetch("max").to_f
+  @num = rand(@min .. @max)
+erb(:randomness)
 end
 get("/") do
   "
